b7e443f988ce9b2824a23cce74d8bcecbeb2bdc9
Menno Finlay-Smits <inbox@menno.io>
2018-04-10 15:35:07 +1200

    spouttest: Remove unused AssertRecvMulti helper
    All usage of this have been replaced by AssertMonitor.

--- spouttest/asserts.go
+++ spouttest/asserts.go
@@ -24,26 +24,6 @@ func AssertRecv(t *testing.T, ch <-chan string, label, expected string) {
 	}
 }
 
-// AssertRecvMulti checks that a specific string has been received
-// from a channel. The channel will be read multiple times if
-// required. The check times out after LongWait.
-func AssertRecvMulti(t *testing.T, ch <-chan string, label, expected string) {
-	expected = stripLeadingNL(expected)
-
-	var received string
-	timeout := time.After(LongWait)
-	for {
-		select {
-		case received = <-ch:
-			if expected == received {
-				return
-			}
-		case <-timeout:
-			t.Fatalf("timed out waiting for %s. last received: %q", label, received)
-		}
-	}
-}
-
 func stripLeadingNL(s string) string {
 	// This allows long `expected` strings to be formatted nicely in
 	// the caller.
