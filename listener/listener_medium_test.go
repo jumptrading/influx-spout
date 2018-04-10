7a80da7318970ec562e1d02ab70f0e145244ce12
Menno Finlay-Smits <inbox@menno.io>
2018-04-10 13:56:04 +1200

    listener: Convert to sending Prometheus metrics to monitor subject

--- listener/listener_medium_test.go
+++ listener/listener_medium_test.go
@@ -22,6 +22,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -295,19 +296,42 @@ func assertNoMore(t *testing.T, ch chan string) {
 }
 
 func assertMonitor(t *testing.T, monitorCh chan string, received, sent int) {
-	expected := fmt.Sprintf(
-		"spout_stat_listener,listener=testlistener received=%d,sent=%d,read_errors=0\n",
-		received, sent)
-	var line string
+	remaining := map[string]bool{
+		fmt.Sprintf(`received{listener="testlistener"} %d`, received): true,
+		fmt.Sprintf(`sent{listener="testlistener"} %d`, sent):         true,
+		`read_errors{listener="testlistener"} 0`:                      true,
+	}
+
+	var allLines string
 	timeout := time.After(spouttest.LongWait)
 	for {
 		select {
-		case line = <-monitorCh:
-			if line == expected {
+		case lines := <-monitorCh:
+			for _, line := range strings.Split(lines, "\n") {
+				if len(line) == 0 {
+					continue
+				}
+				line = stripTimestamp(t, line)
+				allLines += fmt.Sprintf("%q\n", line)
+				delete(remaining, line)
+			}
+			if len(remaining) < 1 {
 				return
 			}
 		case <-timeout:
-			t.Fatalf("timed out waiting for expected stats. last received: %v", line)
+			t.Fatalf("timed out waiting for expected stats. received: %s", allLines)
 		}
 	}
+}
+
+func stripTimestamp(t *testing.T, s string) string {
+	i := strings.LastIndexByte(s, ' ')
+	require.True(t, i >= 0)
+
+	// Check that end looks like a timestamp
+	_, err := strconv.Atoi(s[i+1:])
+	require.NoError(t, err)
+
+	// Strip off the timestamp
+	return s[:i]
 }
